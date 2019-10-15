5c19df847ff1b1a641144d1229e2edf68cecaf69
Alex Ozer <alexozer@users.noreply.github.com>
2019-10-15 17:17:03 -0400

    Make Escape key print screen, for easy screenshots

--- keyboards/kbdfans/kbd75/keymaps/alexozer/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/alexozer/keymap.c
@@ -3,12 +3,12 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Symbols enabled
   [0] = LAYOUT(
-    KC_ESC,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_VOLD,  KC_VOLU,  KC_DEL,
+    KC_PSCR,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_VOLD,  KC_VOLU,  KC_DEL,
     KC_GRV,   KC_1,  KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_DEL,   KC_BSPC,  KC_HOME,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGUP,
     KC_ESC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                      KC_ENT,   KC_PGDN,
     KC_LSFT,  KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_END,
-    KC_LCTL,  KC_LALT,  KC_LGUI,  KC_SPC,   KC_RGUI,   KC_RGUI,                       LT(1, KC_MPLY), MT(MOD_RALT, KC_MPRV),  MT(MOD_RCTL, KC_MNXT),  KC_LEFT,  KC_DOWN,  KC_RGHT
+    KC_LCTL,  KC_LALT,  KC_LGUI,  KC_SPC,   MO(2),   KC_RGUI,     LT(1, KC_MPLY), MT(MOD_RALT, KC_MPRV),  MT(MOD_RCTL, KC_MNXT),  KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
 
   // Symbols disabled
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /*   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_MPRV,  KC_MNXT,  KC_MPLY,            KC_PGUP, */
   /*   KC_ESC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                      KC_ENT,   KC_PGDN, */
   /*   KC_LSFT,  KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_END, */
-  /*   KC_LCTL,  KC_LALT,  KC_LGUI,  KC_SPC,   MO(2),   KC_RGUI,                       MO(1), KC_RALT,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT */
+  /*   KC_LCTL,  KC_LALT,  KC_LGUI,  KC_SPC,   MO(2),   KC_RGUI,     LT(1, KC_MPLY), MT(MOD_RALT, KC_MPRV),  MT(MOD_RCTL, KC_MNXT),  KC_LEFT,  KC_DOWN,  KC_RGHT */
   /* ), */
 
   [1] = LAYOUT(
@@ -35,8 +35,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
     _______,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   _______,  _______,  KC_LBRC,  KC_RBRC,  KC_UNDS,  KC_PLUS,  _______,  _______,  _______,            _______,
-    _______,  KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  _______,  KC_DQUO,  KC_LPRN,  KC_RPRN,  KC_MINS,  KC_EQL,   _______,                      _______,  _______,
-    _______,  _______,  _______,  _______,  KC_GRV,   KC_TILD,  _______,  _______,  KC_LCBR,  KC_RCBR,  KC_PIPE,  KC_BSLS,  _______,  _______,            _______,
+    _______,  KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  _______,  _______,  KC_LPRN,  KC_RPRN,  KC_MINS,  KC_COLN,  KC_DQUO,                      _______,  _______,
+    _______,  _______,  _______,  _______,  KC_GRV,   KC_TILD,  _______,  KC_EQL,  KC_LCBR,  KC_RCBR,  KC_PIPE,  KC_BSLS,  _______,  _______,            _______,
     _______,  _______,  _______,                      _______,  _______,  _______,                      _______,  _______,  _______,  _______,  _______,  _______
   ),
 
@@ -51,6 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* ), */
 };
 
+// Debug stuff
 /* void keyboard_post_init_user(void) { */
 /*   // Customise these values to desired behaviour */
 /*   debug_enable=true; */
@@ -59,37 +60,81 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*   //debug_mouse=true; */
 /* } */
 
-// Disable certain key symbols to help me remember to use the symbol layer
-/* bool process_record_user(uint16_t keycode, keyrecord_t *record) { */
-/*   return true; */
+// Tmux key stuff
+
+/* static bool tmux_key_held = false; */
+/*  */
+/* void register_shifted(uint16_t keycode) { */
+/*   register_code(KC_LSFT); */
+/*   register_code(keycode); */
+/*   unregister_code(keycode); */
+/*   unregister_code(KC_LSFT); */
 /* } */
 
-    /* case KC_1 ... KC_0:  */
-    /* #<{(| case KC_QUOT:  |)}># */
-    /* #<{(| case KC_SCLN: |)}># */
-    /*  */
-    /*   { */
-    /*   bool lshift = get_mods() & (MOD_BIT(KC_LSHIFT)); */
-    /*   bool rshift = get_mods() & (MOD_BIT(KC_RSHIFT)); */
-    /*  */
-    /*   if (lshift) unregister_code(KC_LSHIFT); */
-    /*   if (rshift) unregister_code(KC_RSHIFT); */
-    /*  */
-    /*   if (record->event.pressed) { */
-    /*     register_code(keycode); */
-    /*   } else { */
-    /*     unregister_code(keycode); */
-    /*   } */
-    /*  */
-    /*   if (lshift) register_code(KC_LSHIFT); */
-    /*   if (rshift) register_code(KC_RSHIFT); */
-    /*  */
-    /*   return false; */
-    /* } */
+/* bool process_record_user(uint16_t keycode, keyrecord_t *record) { */
+/*   if (keycode == KC_INT1) { */
+/*     tmux_key_held = record->event.pressed; */
+/*     return false; */
+/*   } */
+/*  */
+/*   if (tmux_key_held && record->event.pressed) { */
+/*     register_code(KC_LCTL); */
+/*     register_code(KC_B); */
+/*     unregister_code(KC_B); */
+/*     unregister_code(KC_LCTL); */
+/*  */
+/*     switch (keycode) { */
+/*       case KC_F1 ... KC_F10: */
+/*         register_shifted(KC_1 + keycode - KC_F1); */
+/*         return false; */
+/*  */
+/*       case KC_F11: */
+/*         register_code(KC_MINS); */
+/*         unregister_code(KC_MINS); */
+/*         return false; */
+/*  */
+/*       case KC_F12: */
+/*         register_code(KC_EQL); */
+/*         unregister_code(KC_EQL); */
+/*         return false; */
+/*  */
+/*       case KC_CALC: */
+/*         register_shifted(KC_TILD); */
+/*         return false; */
+/*     } */
 /*   } */
 /*  */
 /*   return true; */
 /* } */
 
+// Disable some symbols
+/* bool process_record_user(uint16_t keycode, keyrecord_t *record) { */
+/*   switch (keycode) { */
+/*     case KC_1 ... KC_0:  */
+/*     #<{(| case KC_QUOT:  |)}># */
+/*     #<{(| case KC_SCLN: |)}># */
+/*  */
+/*       { */
+/*       bool lshift = get_mods() & (MOD_BIT(KC_LSHIFT)); */
+/*       bool rshift = get_mods() & (MOD_BIT(KC_RSHIFT)); */
+/*  */
+/*       if (lshift) unregister_code(KC_LSHIFT); */
+/*       if (rshift) unregister_code(KC_RSHIFT); */
+/*  */
+/*       if (record->event.pressed) { */
+/*         register_code(keycode); */
+/*       } else { */
+/*         unregister_code(keycode); */
+/*       } */
+/*  */
+/*       if (lshift) register_code(KC_LSHIFT); */
+/*       if (rshift) register_code(KC_RSHIFT); */
+/*  */
+/*       return false; */
+/*     } */
+/*   } */
+/*  */
+/*   return true; */
+/* } */
 
 // vim:nowrap
